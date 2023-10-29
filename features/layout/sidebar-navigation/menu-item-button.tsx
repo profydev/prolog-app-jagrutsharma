a2b6ecba91bcacefd08dd45814a2ec81be26c9f4
Jagrut Sharma <jagrutsharma@gmail.com>
2023-10-28 19:17:34 -0700

    kanban solution (fix sidebar navigation arrow)

--- features/layout/sidebar-navigation/menu-item-button.tsx
+++ features/layout/sidebar-navigation/menu-item-button.tsx
@@ -18,13 +18,33 @@ export function MenuItemButton({
   iconSrc,
   isCollapsed,
 }: MenuItemProps) {
-  return (
-    <li className={classNames(styles.listItem, className)}>
-      <Button className={styles.anchor} onClick={onClick}>
-        {/* eslint-disable-next-line @next/next/no-img-element */}
-        <img className={styles.icon} src={iconSrc} alt={`${text} icon`} />{" "}
-        {!isCollapsed && text}{" "}
-      </Button>
-    </li>
-  );
+  if (!isCollapsed) {
+    return (
+      <li className={classNames(styles.listItem, className)}>
+        <Button className={styles.anchor} onClick={onClick}>
+          {/* eslint-disable-next-line @next/next/no-img-element */}
+          <img
+            className={styles.icon}
+            src={iconSrc}
+            alt={`${text} icon`}
+          />{" "}
+          {!isCollapsed && text}{" "}
+        </Button>
+      </li>
+    );
+  } else {
+    return (
+      <li className={classNames(styles.listItem, className)}>
+        <Button className={styles.anchor} onClick={onClick}>
+          {/* eslint-disable-next-line @next/next/no-img-element */}
+          <img
+            className={styles.righticon}
+            src={iconSrc}
+            alt={`${text} icon`}
+          />{" "}
+          {!isCollapsed && text}{" "}
+        </Button>
+      </li>
+    );
+  }
 }
